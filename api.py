2caa97ada30c41d4dc7b54d66427bdcd7a5fc3de
yachaverrc <yhilmarchaverra@seti.com.co>
2025-04-28 07:49:58 -0500

    actualizacion db json

--- api.py
+++ api.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from dotenv import set_key
 from chatbot import responder_chatbot, ejecutar_comando_directo
 
 app = FastAPI()
-
-# Montar archivos estáticos (HTML)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
+def get_login():
+    with open("static/login.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+@app.post("/guardar_nombre")
+async def guardar_nombre(request: Request):
+    data = await request.json()
+    nombre = data.get("username", "")
+    if nombre:
+        set_key(".env", "USERNAME", nombre)
+    return {"ok": True}
+
+@app.get("/terminal", response_class=HTMLResponse)
 def get_terminal():
     with open("static/terminal.html", "r", encoding="utf-8") as f:
         return f.read()
@@ -20,6 +32,7 @@ async def procesar_mensaje(request: Request):
     
     if mensaje.strip().lower().startswith("cmd "):
         comando = mensaje[4:]
+        print(comando)
         salida = ejecutar_comando_directo(comando)
         return {"respuesta": salida}
     else:
